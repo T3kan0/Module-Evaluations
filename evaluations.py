42cebf48afb87977944590d3a0c1d55047f3a08e
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2023-10-28 20:45:30 +0200

    Update evaluations.py

--- evaluations.py
+++ evaluations.py
@@ -850,7 +850,7 @@ tutors.')
                 btn = st.download_button(
                 label=":red[Download PDF Report]",
                 data=file,
-                file_name='A_STEP_IR_2019_2022.pdf',
+                file_name='A_STEP_'+str(f_name[0])+'ER_2232.pdf',
                 mime="file/pdf"
                   )  
                 st.success('Report Ready for Download', icon="✅")
