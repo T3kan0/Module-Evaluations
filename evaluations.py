f3dc1fdd6b61e75c5ab73135f7d29de62f037153
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-08-29 10:53:54 +0200

    Update evaluations.py

--- evaluations.py
+++ evaluations.py
@@ -19,9 +19,19 @@ from collections import Counter
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-# Download Punkt tokenizer if not already present
-nltk.download('punkt')
+import os
 import string
+
+# Set a directory for NLTK data
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir)
+
+# Download 'punkt' to that directory
+nltk.download('punkt', download_dir=nltk_data_dir)
+
+# Tell NLTK to look there
+nltk.data.path.append(nltk_data_dir)
 
 st.set_page_config(
     page_title="Evaluations",
