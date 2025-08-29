d460158b8a4399cec59ce21354184e866c7c76f2
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-08-29 11:00:07 +0200

    Update evaluations.py

--- evaluations.py
+++ evaluations.py
@@ -17,20 +17,11 @@ import seaborn as sns
 from wordcloud import WordCloud
 from collections import Counter
 import nltk
+nltk.download('punkt_tab')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import os
 import string
-
-# Set up a local nltk_data directory inside your app
-nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
-os.makedirs(nltk_data_dir, exist_ok=True)
-
-# Download 'punkt' to that directory
-nltk.download('punkt', download_dir=nltk_data_dir)
-
-# Append the folder to NLTK's search path
-nltk.data.path.append(nltk_data_dir)
 
 st.set_page_config(
     page_title="Evaluations",
