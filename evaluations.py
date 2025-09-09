abe4e891bcee625becb820f9ecd4e9fa5e347581
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-09-09 18:15:00 +0200

    Update evaluations.py

--- evaluations.py
+++ evaluations.py
@@ -1689,8 +1689,7 @@ teaching, learning and more. Additionally, we studied responses to identify atte
             "Q3: My tutor recognized that students are different",
             "Q4: My tutor made an effort to help me",
             "Q5: My tutor gave me confidence to complete my assessment",
-            "Q6: My tutor was interested in my academic progress",
-            "Q7: My tutor allowed me to use my home language to express myself"
+            "Q6: My tutor was interested in my academic progress"
         ]
         # Title
         pdf.cell(200, 10, "Tutorial Quality Distribution", ln=True, align="C")
@@ -1714,7 +1713,7 @@ teaching, learning and more. Additionally, we studied responses to identify atte
         pdf.cell(0, 5, txt = '3.1.10.1. My tutor was on time?', ln =14, align = 'L')                
         pdf.ln(0.25)
         s22 = 'punc.png'
-        pdf.image(str(s22), x = 50, y = 120, w = 100, h = 70, type = 'PNG')
+        pdf.image(str(s22), x = 50, y = 130, w = 100, h = 70, type = 'PNG')
         pdf.ln(0.25)
         pdf.cell(0, 5, txt = '', ln =20, align = 'C')
         pdf.cell(0, 5, txt = '', ln =20, align = 'C')
@@ -1734,7 +1733,7 @@ teaching, learning and more. Additionally, we studied responses to identify atte
         pdf.cell(0, 5, txt = '3.1.10.2. My tutor helped me with difficulties I encountered in the module?', ln =14, align = 'L')        
         pdf.ln(0.25)
         s23 = 'diffc.png'
-        pdf.image(str(s23), x = 50, y = 200, w = 100, h = 70, type = 'PNG')
+        pdf.image(str(s23), x = 50, y = 210, w = 100, h = 70, type = 'PNG')
         pdf.ln(0.25)
         pdf.cell(0, 5, txt = '', ln =20, align = 'C')
         pdf.cell(0, 5, txt = '', ln =20, align = 'C')
