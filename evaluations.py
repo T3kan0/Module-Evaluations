a5179cf6ece299a4815c2687027957babb1ae7e2
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-09-13 09:54:37 +0200

    Update evaluations.py

--- evaluations.py
+++ evaluations.py
@@ -335,7 +335,11 @@ if eval_files is not None:
                 ha='center', 
                 va='bottom'
             )
+        # Remove plot borders (spines)
+        for spine in ax.spines.values():
+            spine.set_visible(False)
 
+        
         # Show plot
         plt.xticks(rotation=25)
         plt.legend()
